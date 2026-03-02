3fa77bc4526121c84f73e579b1a5d195af83d455
Rehan Haider <rhaider1208@gmail.com>
2026-03-02 17:21:12 +0500

    please

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@
 import { useState } from 'react';
 import Sidebar from '@/components/Sidebar';
 import Header from '@/components/Header';
-import DashboardPage from '@/pages/DashboardPage';
+import DashboardPage from '@/components/DashboardPage';
 
 export default function Home() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
